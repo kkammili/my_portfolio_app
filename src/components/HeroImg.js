7a45528c1a01ad4eb3a0a8fba67250cf252cc2e8
krishna <kkrajus777@gmail.com>
2019-12-21 18:51:40 -0600

    feat(HeroImg): bird images and progress view done

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {withStyles} from '@material-ui/core/styles/index'
 import PropTypes from 'prop-types'
-const myPaint = require('../mp34/kittu.jpg')
+// import PlayArrowIcon from '@material-ui/icons/PlayArrowOutlined'
+// import PauseArrowIcon from '@material-ui/icons/PauseCircleFilledOutlined'
+import Grid from '@material-ui/core/Grid'
+// const myPaint = require('../mp34/kittu.jpg')
+const bird0 = require('../mp34/birds.jpg')
+const bird1 = require('../mp34/birds1.jpg')
+const bird2 = require('../mp34/birds2.jpg')
+
+require('../CSS/Progress.css') //eslint-disable-line
 
 const styles = theme => ({
   card: {
@@ -18,22 +26,72 @@ class HeroImg extends React.Component {
     isMobile: PropTypes.bool
   }
 
+  state={
+    progress: 0
+  }
+
+  startTimer =() => {
+    this.progressTimer = setInterval(() => {
+      if (this.state.progress < 2) {
+        this.setState({progress: this.state.progress + 1})
+      } else {
+        this.setState(({progress: 0}))
+      }
+    }, 5000)
+  }
+
+  componentDidMount () {
+    this.startTimer()
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.progressTimer)
+  }
+
+    handleProgressClick=(e) => {
+      clearInterval(this.progressTimer)
+      this.startTimer()
+      this.setState({
+        progress: parseInt(e.target.id)
+      })
+    }
+
+  birdsArray = [bird0, bird1, bird2]
   render () {
-    const {classes, isMobile} = this.props
+    const {classes} = this.props
     return (
-      <a className={classes.bgColor} name='Home'>
-        <img
-          src={myPaint}
-          alt={'#myPaint'}
-          key={'myPaint'}
-          style={isMobile
-            ? {height: classes.heroImageSm, width: '100%'} : {
-              height: classes.heroImageLg,
-              width: '100%'
-            }
-          }
-        />
-      </a>
+      <Fragment>
+        <a className={classes.bgColor} name='Home'>
+          <img
+            src={this.birdsArray[this.state.progress]}
+            alt={'#myPaint'}
+            key={'myPaint'}
+            style={{height: '50vh', width: '100%'}}
+          />
+        </a>
+
+        <div className={classes.bgColor} style={{paddingTop: 20}} />
+        <Grid className={classes.bgColor} container>
+          <Grid item xs={2} />
+          <Grid item xs={8}>
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+              <div id={0} onClick={(e) => this.handleProgressClick(e)} className={'barOutlined'}>
+                <div className={(this.state.progress === 0) ? 'fill' : null} />
+              </div>
+
+              <div id={1} onClick={(e) => this.handleProgressClick(e)} className={'barOutlined'}>
+                <div className={(this.state.progress === 1) ? 'fill' : null} />
+              </div>
+
+              <div id={2} onClick={(e) => this.handleProgressClick(e)} className={'barOutlined'}>
+                <div className={(this.state.progress === 2) ? 'fill' : null} />
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={2} />
+        </Grid>
+      </Fragment>
+
     )
   }
 }
