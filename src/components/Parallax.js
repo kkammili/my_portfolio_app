f449232dd598dd234f4849851e5b7cbca8a5517b
krishna <kkrajus777@gmail.com>
2020-05-25 23:49:02 -0500

    feat(Parallax): desktop view using parallax effect

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 
 export class Parallax extends Component {
   static propTypes = {
     paragraph: PropTypes.string,
-    image: PropTypes.string
+    image: PropTypes.string,
+    imageAligned: PropTypes.string,
+    headingColor: PropTypes.string,
+    textColor: PropTypes.string,
+    heading: PropTypes.string
   }
 
   render () {
@@ -19,15 +23,71 @@ export class Parallax extends Component {
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
+            fontFamily: 'Times,serif',
+            fontSize: 22,
             color: 'white',
-            fontSize: 36,
+            fontWeight: 'bold',
+            fontStyle: 'normal',
+            textDecoration: 'none',
             display: 'flex'
           }}
         >
-          <div style={{width: window.innerWidth * 0.70}} />
-          <div style={{width: window.innerWidth * 0.30, display: 'flex', alignItems: 'center'}}>
-            {this.props.paragraph}
-          </div>
+          {this.props.imageAligned === 'right' && (
+            <Fragment>
+              <div style={{width: window.innerWidth * 0.30, color: this.props.textColor, height: window.innerHeight}}>
+                <div style={{display: 'flex', alignItems: 'center', height: window.innerHeight}}>
+                  <div style={{marginLeft: 30, textAlign: 'justify'}}>
+                    <span style={{fontSize: 30, color: this.props.headingColor, textDecoration: 'underline'}}>
+                      {this.props.heading}
+                    </span>
+                    <br />
+                    <span>
+                      {this.props.paragraph.split(' ').map((item, index) => {
+                        if (item === '/n') {
+                          return (
+                            <Fragment key={item + index}>
+                              <br />
+                              <br />
+                            </Fragment>
+                          )
+                        } else {
+                          return <span key={item + index}>{item + ' '}</span>
+                        }
+                      })}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div style={{width: window.innerWidth * 0.70}} />
+            </Fragment>
+          )}
+          {this.props.imageAligned === 'left' && (
+            <Fragment>
+              <div style={{width: window.innerWidth * 0.70}} />
+              <div style={{width: window.innerWidth * 0.30, color: this.props.textColor, height: window.innerHeight}}>
+                <div style={{display: 'flex', alignItems: 'center', height: window.innerHeight}}>
+                  <div style={{marginRight: 30, textAlign: 'justify'}}>
+                    <span style={{fontSize: 30, color: this.props.headingColor, textDecoration: 'underline'}}>
+                      {this.props.heading}
+                    </span>
+                    <br />
+                    {this.props.paragraph.split(' ').map((item, index) => {
+                      if (item === '/n') {
+                        return (
+                          <Fragment key={item + index}>
+                            <br />
+                            <br />
+                          </Fragment>
+                        )
+                      } else {
+                        return <span key={item + index}>{item + ' '}</span>
+                      }
+                    })}
+                  </div>
+                </div>
+              </div>
+            </Fragment>
+          )}
         </div>
       </div>
     )
--- src/components/ParallaxAlligner.js
+++ src/components/ParallaxAlligner.js
@@ -1,13 +1,12 @@
 import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 import Parallax from './Parallax'
-const UIS = require('../mp34/editeduis.png')
 
-// import paragraph1, {paragraph2, paragraph3, paragraph4} from './Paragraphs'
-// const C2Img = require('../mp34/ClearC2Img.png')
-// const ReactJS = require('../mp34/React.jpg')
-// const MNEW = require('../mp34/MernNew.png')
-// const hobbies = require('../mp34/hobbies.png')
+import paragraph1, {paragraph2, paragraph3, paragraph4} from './Paragraphs'
+const UIS = require('../mp34/editeduis.png')
+const C2Img = require('../mp34/editedClearC2Img.png')
+const MNEW = require('../mp34/editedmernNew.png')
+const hobbies = require('../mp34/editedHobbies.png')
 // const mern = require('../mp34/mern.jpg')
 
 export class ParallaxAlligner extends Component {
@@ -20,10 +19,33 @@ export class ParallaxAlligner extends Component {
       <Fragment>
         <Parallax
           image={UIS}
-          paragraph={
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
-                ' Consequatur harum natus, odit repudiandae rerum saepe? A ad aliquid assumenda, earum, facere' +
-                ' molestiae necessitatibus officia, quas quidem saepe ullam unde! Incidunt!'}
+          heading={'UIS'}
+          textColor={'#FFD662FF'}
+          headingColor={'#FFD662FF'}
+          imageAligned={'left'}
+          paragraph={paragraph1}
+        />
+        <Parallax
+          image={C2Img}
+          heading={'ClearC2 Inc'}
+          textColor={'#FFFFFF'}
+          headingColor={'#FFFFFF'}
+          imageAligned={'right'}
+          paragraph={paragraph2}
+        /><Parallax
+          image={MNEW}
+          heading={'MERN'}
+          textColor={'#4CB346'}
+          headingColor={'#4CB346'}
+          imageAligned={'left'}
+          paragraph={paragraph3}
+        /><Parallax
+          image={hobbies}
+          heading={'HOBBIES'}
+          textColor={'#00bfff'}
+          headingColor={'#00bfff'}
+          imageAligned={'right'}
+          paragraph={paragraph4}
         />
       </Fragment>
     )
